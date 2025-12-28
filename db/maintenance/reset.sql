210b7294b1dd8033567d6fcd4131006d07193a96
AR-92 <electro.electra@gmail.com>
2025-12-28 13:24:30 +0300

    Update reset script to also delete all users from auth.users

--- db/maintenance/reset.sql
+++ db/maintenance/reset.sql
@@ -2,6 +2,9 @@
 -- This script completely resets the database by dropping and recreating the public schema
 -- WARNING: This will delete all data and schema objects in the database
 
+-- Delete all users from auth schema
+DELETE FROM auth.users;
+
 -- Drop the public schema and all its contents
 DROP SCHEMA IF EXISTS public CASCADE;
 

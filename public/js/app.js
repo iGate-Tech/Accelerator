d810feeb79de667bdd6dbd29765e4304bee7c472
AR-92 <electro.electra@gmail.com>
2025-12-31 06:22:50 +0300

    Revert to esm.sh import for @signaldb/core to resolve dependency issues

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // app.js - Client-side logic for theme, language, and data management
 
-import { Collection } from '/node_modules/@signaldb/core/dist/index.mjs';
+import { Collection } from 'https://esm.sh/@signaldb/core';
 
 // Collections
 const nodesCollection = new Collection('nodes');

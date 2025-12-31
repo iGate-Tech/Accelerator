1320b7c0e5ed26979e35358be782732461c9ac88
AR-92 <electro.electra@gmail.com>
2025-12-31 05:36:42 +0300

    Switch to signaldb via esm.sh CDN for client-side data management

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // app.js - Client-side logic for theme, language, and data management
 
-import { createCollection } from 'https://cdn.skypack.dev/@signaldb/core';
+import { createCollection } from 'https://esm.sh/signaldb';
 
 // Collections
 const nodesCollection = createCollection('nodes');
---
+++ public/js/signaldb.min.js
@@ -0,0 +1,30 @@
+import { default as default2 } from "./index2.mjs";
+import { default as default3 } from "./index3.mjs";
+import { default as default4 } from "./index4.mjs";
+import { default as default5 } from "./index5.mjs";
+import { default as default6 } from "./index6.mjs";
+import { default as default7 } from "./index7.mjs";
+import { default as default8 } from "./index8.mjs";
+import { default as default9 } from "./index9.mjs";
+import { default as default10 } from "./index10.mjs";
+import { default as default11 } from "./index11.mjs";
+import { default as default12 } from "./index12.mjs";
+import { default as default13 } from "./index13.mjs";
+import { default as default14 } from "./index14.mjs";
+import { default as default15 } from "./index15.mjs";
+export {
+  default5 as AutoFetchCollection,
+  default2 as Collection,
+  default3 as PersistentCollection,
+  default4 as ReplicatedCollection,
+  default14 as SyncManager,
+  default10 as combinePersistenceAdapters,
+  default8 as createFilesystemAdapter,
+  default15 as createIndex,
+  default13 as createIndexProvider,
+  default6 as createLocalStorageAdapter,
+  default11 as createMemoryAdapter,
+  default7 as createOPFSAdapter,
+  default9 as createPersistenceAdapter,
+  default12 as createReactivityAdapter
+};

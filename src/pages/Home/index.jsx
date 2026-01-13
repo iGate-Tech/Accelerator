ebea1b7e58a138f1018204e8fbb3984b89153b77
ahmed rana <electro.electra@gmail.com>
2026-01-13 18:28:37 +0300

    Fix isOnline reference error in handleLLMCall
    - Import isOnline from useOnlineStatus hook
    - Ensure internet connection check works properly

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -80,6 +80,7 @@ const TasksContent = () => {
     const { lang } = useContext(LangContext);
     const { user } = useUser();
     const activityLogger = useActivityLogger();
+    const { isOnline } = useOnlineStatus();
     const [currentLang, setCurrentLang] = createSignal(lang());
 
     const t = () => translations[currentLang()];
